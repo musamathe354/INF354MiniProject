f0a6116897cd5d114a29492a976a3490c161dc0b
Akua Afrane-Okese <36995926+AkuaB@users.noreply.github.com>
2018-05-03 10:35:35 +0200

    Update to sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,8 @@
 --DROP DATABASE furnitureShop
 CREATE DATABASE furnitureShop
 
+USE furnitureShop
+
 CREATE TABLE ClientTitle
 (
 	CT_ID int identity(01,1) PRIMARY KEY NOT NULL,
@@ -14,7 +16,7 @@ CREATE TABLE Client
 	Surname VARCHAR(255) NOT NULL,
 	IDNum int NOT NULL,
 	
-	FOREIGN KEY (CT_ID) INT REFERENCES ClientTitle(CT_ID) NOT NULL
+	(CT_ID) INT REFERENCES ClientTitle(CT_ID) NOT NULL
 )
 
 CREATE TABLE FurnitureType
@@ -28,7 +30,30 @@ CREATE TABLE Furniture
 	F_ID int identity(1,1) PRIMARY KEY NOT NULL,
 	F_Name VARCHAR(255),
 	Quantity int,
-	F_Type VARCHAR(255),
-	FOREIGN KEY (F_Type) REFERENCES FurnitureType(Furn_Type)
+	(F_ID) INT REFERENCES FurnitureType(F_ID)
 )
+
+INSERT INTO ClientTitle(Title)
+VALUES ('Mr'), ('Miss')
+
+INSERT INTO Client(Name,Surname,IDNum,CT_ID)
+VALUES ('Musa','Mathe', 0897856, 1)
+
+INSERT INTO FurnitureType(Furn_Type)
+VALUES ('Kitchen'), ('Living Room')
+
+INSERT INTO Furniture(F_Name, Quantity, F_ID)
+VALUES ('Lounge table', 18, 1)
+
+SELECT * FROM ClientTitle
+GO
+
+SELECT * FROM Client
+GO
+
+SELECT * FROM FurnitureType
+GO
+
+SELECT * FROM Furniture
+GO
 
